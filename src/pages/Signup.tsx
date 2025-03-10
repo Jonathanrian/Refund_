ca9a71d4892cde64b2916b36f06d22d43dffd8b0
jonathanrian <jonathasryann@gmail.com>
2025-03-10 14:50:07 -0300

    commit: "registering user in the api"

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,23 +1,68 @@
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
 
+import { AxiosError } from "axios";
 import { useState } from "react";
+import { z, ZodError } from "zod";
+import { useNavigate } from "react-router";
+
+import { api } from "../services/api";
+
+const signupSchema = z
+  .object({
+    name: z.string().min(1, { message: "Informe o nome" }),
+    email: z.string().email({ message: "E-mail inválido" }),
+    password: z
+      .string()
+      .min(6, { message: "Senha deve ter no mínimo 6 caracteres" }),
+    passwordConfirm: z.string({ message: "Confirme a senha" }),
+  })
+  .refine((data) => data.password === data.passwordConfirm, {
+    message: "Senhas não conferem",
+    path: ["passwordConfirm"],
+  });
 
 export function Signup() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-  function onSubmit(e: React.FormEvent) {
+  const navigate = useNavigate();
+
+  async function onSubmit(e: React.FormEvent) {
     e.preventDefault();
 
-    console.log(name, email, password, confirmPassword);
+    try {
+      setIsLoading(true);
 
-    setEmail("");
-    setPassword("");
-    setConfirmPassword("");
-    setName("");
+      const data = signupSchema.parse({
+        name,
+        email,
+        password,
+        passwordConfirm,
+      });
+
+      await api.post("/users", data);
+
+      if (confirm("Cadastrado com sucesso. Clique em OK para continuar.")) {
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+      if (error instanceof ZodError) {
+        return alert(error.issues[0].message);
+      }
+
+      if (error instanceof AxiosError) {
+        return alert(error.response?.data.message);
+      }
+
+      alert("Não possivel cadastrar!");
+    } finally {
+      setIsLoading(false);
+    }
   }
 
   return (
@@ -51,10 +96,12 @@ export function Signup() {
         legend="confirmar senha"
         type="password"
         placeholder="12345"
-        onChange={(e) => setConfirmPassword(e.target.value)}
+        onChange={(e) => setPasswordConfirm(e.target.value)}
       />
 
-      <Button type="submit">Criar conta</Button>
+      <Button type="submit" isLoading={isLoading}>
+        Cadastrar
+      </Button>
 
       <a
         href="/"
